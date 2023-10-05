f397c4165ad2b86cb9e80a4b621550d61bd02be7
AgentOsiol <113114202+AgentOsiol@users.noreply.github.com>
2023-10-05 09:47:15 +0100

    started the buy button

--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -6,8 +6,6 @@ public class PointCounter : MonoBehaviour
 {
     public int Points = 0;
 
-    GameObject Basketball;
-
     // Start is called before the first frame update
     void Start()
     {
