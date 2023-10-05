f7f1c8ca73550fe3f6544824181701ce2b7c2d1e
AgentOsiol <113114202+AgentOsiol@users.noreply.github.com>
2023-10-05 12:48:13 +0100

    added to the shop system

--- Assets/Scripts/BuyButtonScript.cs
+++ Assets/Scripts/BuyButtonScript.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BuyButtonScript : MonoBehaviour
 {
+    //Scripts
+    PointManager pointManger;
+    ShopSystem shopsystem;
+    //ItemID
+    public int ItemID;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +22,27 @@ public class BuyButtonScript : MonoBehaviour
     {
         
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Basketball")
+        {
+            if(ItemID == 1 && GetComponent<PointManager>().Points < 10)
+            {
+                GetComponent<PointManager>().Points -= 10;
+                Debug.Log("A");
+            }
+            else if (ItemID == 2 && GetComponent<PointManager>().Points < 15)
+            {
+                GetComponent<PointManager>().Points -= 15;
+            }
+            else if (ItemID == 3 && GetComponent<PointManager>().Points < 30) 
+            {
+                GetComponent<PointManager>().Points -= 30;
+            }
+        }
+        
+            
+        
+    }
 }
+
--- Assets/Scripts/PointManager.cs
+++ Assets/Scripts/PointManager.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PointCounter : MonoBehaviour
+public class PointManager : MonoBehaviour
 {
     public int Points = 0;
-
+    
     // Start is called before the first frame update
     void Start()
     {
