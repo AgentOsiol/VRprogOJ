1c5e611f64fb772159046afe8592fc1bcb86c509
AgentOsiol <113114202+AgentOsiol@users.noreply.github.com>
2023-11-08 09:56:38 +0000

    Revert "m"
    This reverts commit 2c6e0381b0b214a79a4042eb106c4952bdd67117.

---
+++ Assets/Scripts/BasketballScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BasketballScript : MonoBehaviour
+{
+    MeshRenderer mr;
+    SphereCollider sc;
+    ParticleSystem ps;
+    Rigidbody rb;
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        sc = GetComponent<SphereCollider>();
+        ps = GetComponentInChildren<ParticleSystem>();
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0, -5, 0);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "PointDetector")
+        {
+            StartCoroutine(Db(2f));
+            //components
+            mr.enabled = false;
+            sc.isTrigger = true;
+            rb.isKinematic = true;
+            ps.Play();
+        }
+    }
+
+    public IEnumerator Db(float x)
+    {
+        yield return new WaitForSeconds(x);
+        Debug.Log("destroyed");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BuyButtonScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BuyButtonScript : MonoBehaviour
+{
+    //Scripts
+    PointManager pm;
+    //ItemID
+    public int ItemID;
+    //truefalse
+    bool bought1;
+    bool bought2;
+    bool bought3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pm = FindObjectOfType<PointManager>();
+        bought1 = false;
+        bought2 = false;
+        bought3 = false;
+    }
+
+    public void test()
+    {
+            if(ItemID == 1 && pm.Points >= 5 && bought1 == false)
+            {
+                pm.Points -= 5;
+                Debug.Log("A");
+                bought1 = true;
+            }
+            else if (ItemID == 2 && pm.Points >= 10 && bought2 == false)
+            {
+                pm.Points -= 10;
+                Debug.Log("B");
+                bought2 = true;
+            }
+            else if (ItemID == 3 && pm.Points >= 15 && bought3 == false)
+            {
+                pm.Points -= 15;
+                Debug.Log("C");
+                bought3 = true;
+            }
+    }
+}
