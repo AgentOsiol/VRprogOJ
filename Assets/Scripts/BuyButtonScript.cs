bfe79a8429d6c882e949ea44c8d7aa27ccbb3eb7
AgentOsiol <ntaage295@gmail.com>
2023-11-09 02:43:31 +0000

    finished shop

--- Assets/Scripts/BuyButtonScript.cs
+++ Assets/Scripts/BuyButtonScript.cs
@@ -13,7 +13,10 @@ public class BuyButtonScript : MonoBehaviour
     bool bought1;
     bool bought2;
     bool bought3;
-
+    //gameobjects
+    public GameObject Cap;
+    public GameObject Shoeb;
+    public GameObject Shoew;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,27 +24,48 @@ public class BuyButtonScript : MonoBehaviour
         bought1 = false;
         bought2 = false;
         bought3 = false;
+
+        Cap.SetActive(false);
+        Shoeb.SetActive(false);
+        Shoew.SetActive(false);
     }
 
     public void test()
     {
-            if(ItemID == 1 && pm.Points >= 5 && bought1 == false)
+            if(ItemID == 1 && pm.Points >= 3 && bought1 == false)
             {
-                pm.Points -= 5;
+                pm.Points -= 3;
                 Debug.Log("A");
                 bought1 = true;
             }
-            else if (ItemID == 2 && pm.Points >= 10 && bought2 == false)
+            else if (ItemID == 2 && pm.Points >= 5 && bought2 == false)
             {
-                pm.Points -= 10;
+                pm.Points -= 5;
                 Debug.Log("B");
                 bought2 = true;
             }
-            else if (ItemID == 3 && pm.Points >= 15 && bought3 == false)
+            else if (ItemID == 3 && pm.Points >= 5 && bought3 == false)
             {
-                pm.Points -= 15;
+                pm.Points -= 5;
                 Debug.Log("C");
                 bought3 = true;
             }
+    }
+
+    public void Update()
+    {
+        if(bought1 == true)
+        {
+            Cap.SetActive(true);
+        }
+        else if (bought2 == true)
+        {
+            Shoeb.SetActive(true);
+        }
+        else if (bought3 == true)
+        {
+            Shoew.SetActive(true);
+        }
+
     }
 }
